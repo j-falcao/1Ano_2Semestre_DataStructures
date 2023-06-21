ecaedfdaa578ec03a943ea5c49eb65589d6736e0
j-falcao2002 <joaorpalharesfalcao@gmail.com>
2023-06-21 11:22:57 +0100

    pdf insert

--- Ficha_Hash/FuncoesHashing.c
+++ Ficha_Hash/FuncoesHashing.c
@@ -5,6 +5,10 @@ Hashing* criarVectorHashing(){
 }
 
 void inicializarVectorHashing(Hashing *H){
+    if(!H){
+        printf("\n\tError! Given Hashing is NULL\n");
+        return;
+    }
     for(int i = 0; i < 10; i++){
         H->hash[i]->faixaEtaria = i;
         H->hash[i]->listaPessoas = criarLista();
@@ -12,15 +16,28 @@ void inicializarVectorHashing(Hashing *H){
 }
 
 int posicaoHashingElemento(Elemento *E){
-    
+    return E->pessoa->idade / 10;
 }
 
 int validarPosicaoHashing(int pos){
-
+    if(pos >= 0 && pos <= 9)
+        return 1;
+    return 0;
 }
 
 void inserirElementoHashing(Hashing *H, Elemento *eleInserir){
+    if(!H){
+        printf("\n\tError! Given Hashing is NULL\n");
+        return;
+    }
+    if(!eleInserir){
+        printf("\n\tError! Given element is NULL\n");
+        return;
+    }
+    int posHash = posicaoHashingElemento(eleInserir);
 
+    if(validarPosicaoHashing(posHash))
+        inserirElementoOrdenadoIdade(H->hash[posHash]->listaPessoas, eleInserir);
 }
 
 Elemento* removerElementoHashing(Hashing *H, Elemento *eleRemover){
